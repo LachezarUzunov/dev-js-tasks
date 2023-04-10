b8e265dbd0821a84d2d1daf77e9ea439f960292e
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-04-10 10:31:20 +0300

    completing task 3

--- task4/app.js
+++ task4/app.js
@@ -3,12 +3,13 @@ function solve() {
     .querySelector(".btn")
     .addEventListener("click", checker);
 
-  const para = document.createElement("p");
+  const para = document.querySelector(".text");
+
   function checker(e) {
     const word = e.target.parentElement.children[0].value;
 
-    const reversed = word.split("").reverse().join();
-
+    const reversed = word.split("").reverse().join("");
+    console.log(reversed);
     if (word === reversed) {
       para.textContent = "True, the added word is a palindrome";
     } else {
